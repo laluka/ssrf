6f81a31ee781fea1658ef74f5c6a82a3f1485d68
lalu <loukajc@gmail.com>
2025-03-26 20:59:40 +0100

    update guidelines

--- src/components/StreamGuidelines/guidelinesData.ts
+++ src/components/StreamGuidelines/guidelinesData.ts
@@ -1,11 +1,10 @@
 export const guidelines = {
   'Audio & Video': [
-    { text: 'Discord client SHOULD be installed (way better for video/sound)' },
-    { text: 'Discord WebUI CAN be used as a fallback/backup' },
-    { text: 'Guest MUST have a good microphone and SHOULD have decent webcam' },
+    { text: 'We go Live with restream.io, guests MUST stop their VPN / torrent / proxy / etc' },
+    { text: 'Guests MUST have a good microphone and SHOULD have decent Webcam' },
     { text: 'Avoid as much as possible blueetooth headset / buds, if required, charge them 100%' },
-    { text: 'If interpherences are seen (happened) keep the phone away', emoji: '🥺' },
-    { text: 'A good-enough network & CPU' },
+    { text: 'If interpherences are seen (happened) keep the phone 2m away...', emoji: '🥺' },
+    { text: 'A good-enough network & CPU!' },
     { text: 'A live end-to-end group test a few days before the D-day' },
     {
       text: "Once everything's ok : Don't change a thing, no last minute patch / update / kernel-swap!",
@@ -13,17 +12,16 @@ export const guidelines = {
   ],
   Security: [
     { text: 'Shell history // mv ~/.zsh_history ~/.zsh_history.bak' },
-    { text: 'Turn off any VPN that could introduce network hiccups / leaks' },
     { text: 'No secret should show-up while running "env"' },
-    { text: 'Do-Not-Disturb mode enabled on discord/teams/slack/other' },
-    { text: 'IDE must open blank or cwd, not on "last project open"' },
+    { text: 'Do-Not-Disturb mode enabled on discord/teams/lkd/slack/other' },
+    { text: 'IDE MUST open blank or cwd, not on "last project open"' },
     { text: 'Browser history & url autocompletion (avoid leaking private urls)' },
     { text: 'Default browser page SHOULD not leak your most visited sites' },
   ],
   'Generic Guidelines': [
     { text: 'If you vape, drink, cough, remember to self-mute', emoji: '💕' },
     { text: 'Vape & Alcohol are OK but be discreet (mug)', emoji: '😏' },
-    { text: 'Showcase personal projects is OK, make it smooth ;)' },
+    { text: 'Showcase personal projects is OK, make it smooth!', emoji: '😘' },
   ],
   'Then What': [
     {
