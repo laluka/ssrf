7ab8d4e2eb009bddf027aa6e1d0aada263a1fb2a
laluka <loukajc@gmail.com>
2025-05-03 21:09:41 +0200

    neat

--- update-content.sh
+++ update-content.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # 0 7 * * * bash -x /opt/ssrf/update-content.sh 2>&1 | tee -a /tmp/ssrf.log
+export GIT_SSH_COMMAND='ssh -i /home/lalu/.ssh/ssrf-daily-update-key'
+cd /opt/ssrf && git pull && npm run ytdlp && npm run populate && git add . && git commit -am "MAJ: $(date)" && git push
 
-cd /opt/ssrf && GIT_SSH_COMMAND='ssh -i /home/lalu/.ssh/ssrf-daily-update-key' git pull && npm run ytdlp && npm run populate && git add . && git commit -am "MAJ: $(date)" && git push
